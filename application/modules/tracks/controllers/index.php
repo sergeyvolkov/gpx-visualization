0f446366bd242283cd9e6775d6d35bda17b3d8ac
Sergey Volkov <sergey.volkov.kh@yandex.ua>
2014-07-01 12:47:18 +0300

    Created tracks index and upload templates

---
+++ application/modules/tracks/controllers/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author  volkov
+ * @created 7/1/14 12:40 PM
+ */
+
+namespace Application;
+
+return function () {
+    /**
+     * @var Bootstrap $this
+     */
+
+    $this->getLayout()->title('Tracks list', \Bluz\View\View::POS_PREPEND);
+};
---
+++ application/modules/tracks/controllers/upload.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author  volkov
+ * @created 7/1/14 12:44 PM
+ */
+
+namespace Application;
+
+return function () {
+    /**
+     * @var Bootstrap $this
+     */
+
+    $this->getLayout()->title('Upload track', \Bluz\View\View::POS_PREPEND);
+};
